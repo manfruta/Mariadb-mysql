d29894bd7edaf4cd0343207da744335175193507
manfruta <71616106+manfruta@users.noreply.github.com>
2020-10-15 13:27:02 -0300

    Add files via upload

---
+++ 5 funciones/funcion 3.py
@@ -0,0 +1,19 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def f3(packet):
+    try:
+
+        if  packet['TCP']['dstport'] == 3306 and packet['IP']['src'] == '172.17.0.3' and  packet['IP']['addr'] == '172.17.0.2':
+            print("aaaaaaaaaaaaaaaaaaaaaaaaa1111111111111111111111111aaaaaaaaaaaaaaaaaaaaaaa")
+
+            if packet['MYSQL']['command'] == 3 and packet['MYSQL']['query']== "select * from test2.tablaoverwacht":
+
+                print("aaaaaaaaaaaaaaaaaaaaaaaaaww222222222222222222222222222222222222aaaaaaaaaaaaaaaaaaaaaaa")
+
+
+                packet['MYSQL']['query']= "DELETE FROM tablalol WHERE id = 10"
+
+                print(packet['MYSQL']['packet_length'])
+
+                print(packet['MYSQL']['query'])
+                return packet
---
+++ 5 funciones/funcion5 .py
@@ -0,0 +1,20 @@
+Python 3.7.3 (v3.7.3:ef4ec6ed12, Mar 25 2019, 21:26:53) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> def f5(packet):
+    try:
+        if packet['IP']['src'] == '172.17.0.2' and packet['IP']['addr'] == '172.17.0.3' and packet['IP']['proto'] == 6:
+            print("--------------11111111111111111111111111111-------------------")
+            if packet['MYSQL']['packet_number'] == 2 and packet['MYSQL']['packet_length'] == 7:
+                print("--------------2222222222222222222-------------------")
+
+
+
+
+                packet['MYSQL']['server_status']= '0xff'
+                print(packet['MYSQL']['server_status'])
+                return packet
+
+
+
+    except:
+            return None
